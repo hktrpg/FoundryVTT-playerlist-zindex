541970aa8f640662ab44dcf1a501f1c2fe3d9cc5
Sad <cougar.l@hotmail.com>
2023-02-11 22:18:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ Hooks.on("renderSceneControls", (dialog, $element) => {
   html.style.pointerEvents = 'visible';
   html.style.overflowY = "scroll";
   const style = document.createElement("style");
-  style.innerHTML = ".flexrow#controls::-webkit-scrollbar { display: none; }";
+  style.innerHTML = ".flexrow#controls::-webkit-scrollbar { display: none !important;} .flexrow#controls::-webkit-scrollbar-thumb { display: none !important;}";
+
   document.head.appendChild(style);
 });
